a06ea71207df02453aa3d87c1dcf3e26600cab0d
xasannosirov <xasannosirov094@gmail.com>
2023-12-28 20:17:50 +0500

    changed main.go

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"stepik_golang/concurency"
+	ifstatement "stepik_golang/if_statement"
+)
 
+func main() {
+	thischan := make(chan int, 2)
+	concurency.DddDataChan(thischan, 3)
+	fmt.Println(<-thischan)
+
+	fmt.Println(ifstatement.ReturnData(5))
 }
